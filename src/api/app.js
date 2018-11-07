db0f1eac61f91f053dc1be8385c4b03c37271142
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2018-11-06 20:57:17 -0600

    creacion de endpoint de crear bache

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {Bache} = require('../basedatos/mongoController');
 const app = express();
+
+
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,19 +13,32 @@ app.get('/', (request, response)=>{
     response.status(200).send('Estamos en la !! ....');
 });
 
+// end pint post
+app.post('/api/v1/bache/', (req, res)=>{
+  const {latitud,longitud,comentario} =  req.body;
+  const bacheNuevo = Bache({
+        latitud: latitud,
+        longitud: longitud,
+        comentario: comentario
+    });
+
+    // Pedir a la base de datos que guarde el nuevo objeto en su respectiva colección
+    bacheNuevo.save( (error, nuevoDirector)=>{
+        if (error) {
+            res.status(400).send(error.message);
+        } else {
+            res.status(201).send(nuevoDirector);
+          }
+        });
+});
+
 // end pint list
 app.get('', (req, res)=>{
 
 });
-
-// end pint id
+//
+// // end pint id
 app.get('', (req, res)=>{
-
-});
-
-
-// end pint post
-app.post('', (req, res)=>{
 
 });
 
